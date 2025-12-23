99f74fe24cdf42a0c753831d6f47af8aabec917a
andres <afquinteromoreano@gmail.com>
2025-12-23 15:41:47 -0500

    feat: Improves error messages for cloud secrets

--- src/secrets/gcp.rs
+++ src/secrets/gcp.rs
@@ -1,40 +1,20 @@
 use google_cloud_secretmanager_v1::client::SecretManagerService;
 
-pub async fn fetch_secret(name: &str, version: &str) -> String {
-    let result: Result<String, Box<dyn std::error::Error + Send + Sync>> = async {
-        let client = SecretManagerService::builder().build().await?;
+use crate::service::vars::ServiceConfigError;
 
-        let response = client
-            .access_secret_version()
-            .set_name(format!("{name}/versions/{version}"))
-            .send()
-            .await?;
+pub async fn fetch_secret(name: &str, version: &str) -> Result<String, ServiceConfigError> {
+    let client = SecretManagerService::builder().build().await?;
 
-        let payload = response.payload.ok_or_else(|| {
-            Box::new(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "Secret payload is empty or missing",
-            ))
-        })?;
+    let response = client
+        .access_secret_version()
+        .set_name(format!("{name}/versions/{version}"))
+        .send()
+        .await?;
 
-        let secret_bytes = payload.data;
-        let secret_str = std::str::from_utf8(&secret_bytes)?;
+    let payload = response.payload.unwrap_or_default();
 
-        Ok(secret_str.to_string())
-    }
-    .await;
+    let secret_bytes = payload.data;
+    let secret_str = std::str::from_utf8(&secret_bytes)?;
 
-    match result {
-        Ok(secret) => secret,
-        Err(e) => {
-            // Log the error and return an empty string to make the function "infalible"
-            log::error!(
-                "Error fetching secret '{}/versions/{}': {}",
-                name,
-                version,
-                e
-            );
-            "".to_string()
-        }
-    }
+    Ok(secret_str.to_string())
 }
